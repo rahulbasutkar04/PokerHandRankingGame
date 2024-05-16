b915c446b74d3151942b12fc3a8620a3a31a9b00
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-16 10:08:01 +0530

    Add Custom Exceptions

---
+++ src/main/java/com/amaap/pokerhandranking/service/exception/DuplicateCardException.java
@@ -0,0 +1,7 @@
+package com.amaap.pokerhandranking.service.exception;
+
+public class DuplicateCardException extends Throwable {
+    public DuplicateCardException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/com/amaap/pokerhandranking/service/exception/InvalidCardCountException.java
@@ -0,0 +1,7 @@
+package com.amaap.pokerhandranking.service.exception;
+
+public class InvalidCardCountException extends Exception{
+    public InvalidCardCountException(String message) {
+        super(message);
+    }
+}
