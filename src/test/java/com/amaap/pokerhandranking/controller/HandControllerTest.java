7825ef9ba6a6f6bfa542188d31e5613bfd688fd1
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-16 12:13:09 +0530

    Update code

--- src/test/java/com/amaap/pokerhandranking/controller/HandControllerTest.java
+++ src/test/java/com/amaap/pokerhandranking/controller/HandControllerTest.java
@@ -3,9 +3,9 @@ package com.amaap.pokerhandranking.controller;
 import com.amaap.pokerhandranking.controller.dto.Http;
 import com.amaap.pokerhandranking.controller.dto.Response;
 import com.amaap.pokerhandranking.domain.service.CardParser;
+import com.amaap.pokerhandranking.repository.impl.InMemoryHandRepository;
 import com.amaap.pokerhandranking.service.HandService;
 import com.amaap.pokerhandranking.service.exception.DuplicateCardException;
-import com.amaap.pokerhandranking.service.exception.InvalidCardCountException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,30 +18,33 @@ public class HandControllerTest {
 
     HandService handService;
     CardParser cardParser;
+    InMemoryHandRepository inMemoryHandRepository;
 
     @BeforeEach
-    void setup(){
-        cardParser=new CardParser();
-        handService=new HandService(cardParser);
+    void setup() {
+        cardParser = new CardParser();
+        inMemoryHandRepository = InMemoryHandRepository.getInstance();
+        handService = new HandService(cardParser, inMemoryHandRepository);
+        inMemoryHandRepository.clear();
     }
 
     @Test
     void shouldBeAbleToReceiveCards() throws Exception, DuplicateCardException {
         // arrange
-        HandController handController =new HandController(handService);
-        List<String> receivedCards=new ArrayList<>();
+        HandController handController = new HandController(handService);
+        List<String> receivedCards = new ArrayList<>();
         receivedCards.add("HA");
         receivedCards.add("S2");
         receivedCards.add("D5");
         receivedCards.add("C7");
         receivedCards.add("ST");
 
-        Response expected=new Response(Http.OK,"Cards Submitted");
+        Response expected = new Response(Http.OK, "Cards Submitted");
         // act
-        Response actual= handController.receiveCards(receivedCards);
+        Response actual = handController.receiveCards(receivedCards);
 
         // assert
 
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
     }
 }
