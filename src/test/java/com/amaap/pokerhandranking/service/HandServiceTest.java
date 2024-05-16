f612ea076f3f2f1c48ba69ace6883974e66aa64e
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-16 12:11:01 +0530

    update test case

--- src/test/java/com/amaap/pokerhandranking/service/HandServiceTest.java
+++ src/test/java/com/amaap/pokerhandranking/service/HandServiceTest.java
@@ -1,6 +1,7 @@
 package com.amaap.pokerhandranking.service;
 
 import com.amaap.pokerhandranking.domain.service.CardParser;
+import com.amaap.pokerhandranking.repository.impl.InMemoryHandRepository;
 import com.amaap.pokerhandranking.service.exception.DuplicateCardException;
 import com.amaap.pokerhandranking.service.exception.InvalidCardCountException;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,23 +10,25 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class HandServiceTest {
     CardParser cardParser;
+    HandService handService;
+    InMemoryHandRepository inMemoryHandRepository;
 
     @BeforeEach
-    void setp()
-    {
-        cardParser=new CardParser();
+    void setp() {
+        cardParser = new CardParser();
+        inMemoryHandRepository=InMemoryHandRepository.getInstance();
+        handService=new HandService(cardParser,inMemoryHandRepository);
+        inMemoryHandRepository.clear();
     }
 
 
     @Test
     void shouldBeAbleToReturnTrueIfFiveCardsAreAdded() throws Exception, DuplicateCardException {
         // arrange
-        HandService handService = new HandService(cardParser);
         List<String> receivedCards = new ArrayList<>();
         receivedCards.add("HA");
         receivedCards.add("S2");
@@ -41,7 +44,6 @@ class HandServiceTest {
     @Test
     void shouldThrowExceptionIfMoreThanFiveCardsAreGiven() {
         // arrange
-        HandService handService = new HandService(cardParser);
         List<String> receivedCards = new ArrayList<>();
         receivedCards.add("HA");
         receivedCards.add("S2");
@@ -60,7 +62,6 @@ class HandServiceTest {
     @Test
     void shouldThrowExceptionIfDuplicateCardFound() {
         // arrange
-        HandService handService = new HandService(cardParser);
         List<String> receivedCards = new ArrayList<>();
         receivedCards.add("HA");
         receivedCards.add("S2");
@@ -72,16 +73,55 @@ class HandServiceTest {
         assertThrows(DuplicateCardException.class, () -> {
             handService.receiveCards(receivedCards);
         });
-    }    @Test
+    }
+
+    @Test
     void shouldThrowExceptionIfNoCardIsPassed() {
         // arrange
-        HandService handService = new HandService(cardParser);
         List<String> receivedCards = new ArrayList<>();
 
         // act & assert
         assertThrows(InvalidCardCountException.class, () -> {
             handService.receiveCards(receivedCards);
         });
+    }
+
+    @Test
+    void shouldReturnFalseIfGivenCardListHasInvalidTypeOfCard() throws DuplicateCardException, Exception {
+        // arrange
+        List<String> receivedCards = new ArrayList<>();
+        receivedCards.add("HA");
+        receivedCards.add("S2");
+        receivedCards.add("5D");
+        receivedCards.add("C7");
+        receivedCards.add("ST");
+
+        // act
+        boolean actual = handService.receiveCards(receivedCards);
+
+        // assert
+        assertFalse(actual);
+
+
+    }
+
+    @Test
+    void shouldBeAbelToGetTheStoredCardFromDatabase() throws DuplicateCardException, Exception {
+        // arrange
+        List<String> receivedCards = new ArrayList<>();
+        receivedCards.add("HA");
+        receivedCards.add("S2");
+        receivedCards.add("D5");
+        receivedCards.add("C7");
+        receivedCards.add("ST");
+
+        // act
+        handService.receiveCards(receivedCards);
+       int storedCardSize= inMemoryHandRepository.getCards().size();
+
+       // assert
+       assertEquals(receivedCards.size(),storedCardSize);
+
     }
 
 }
