217f36c6deb854d5979f10171dcd343c7e1d0633
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-16 10:09:08 +0530

    update  HandService class

--- src/main/java/com/amaap/pokerhandranking/service/HandService.java
+++ src/main/java/com/amaap/pokerhandranking/service/HandService.java
@@ -1,9 +1,29 @@
 package com.amaap.pokerhandranking.service;
 
+import com.amaap.pokerhandranking.service.exception.DuplicateCardException;
+import com.amaap.pokerhandranking.service.exception.InvalidCardCountException;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class HandService {
-    public  boolean receiveCards(List<String> cards) {
-        return  true;
+    public boolean receiveCards(List<String> cards) throws InvalidCardCountException, DuplicateCardException {
+        if (cards.size() != 5)
+            throw new InvalidCardCountException("Card count should be 5 your given Card  count is:" + cards.size());
+        List<String> duplicates = findDuplicates(cards);
+        if (!duplicates.isEmpty()) {
+            throw new DuplicateCardException("Duplicate cards found: " + duplicates);
+        }
+
+        return true;
+    }
+
+    private static List<String> findDuplicates(List<String> list) {
+        Set<String> elements = new HashSet<>();
+        return list.stream()
+                .filter(n -> !elements.add(n))
+                .collect(Collectors.toList());
     }
 }
