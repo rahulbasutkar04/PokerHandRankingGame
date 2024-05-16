d4e8e0ddd44e6a9f275ab9dec4ade3ed0ad09bb4
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-16 11:13:00 +0530

    Add custom Exception

---
+++ src/main/java/com/amaap/pokerhandranking/domain/service/exception/InvalidCardTypeException.java
@@ -0,0 +1,7 @@
+package com.amaap.pokerhandranking.domain.service.exception;
+
+public class InvalidCardTypeException extends Exception {
+    public InvalidCardTypeException(String s) {
+        super(s);
+    }
+}
