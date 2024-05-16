121cd4f51bbe95b6e8b196dcf20062ee2a2cb27c
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-16 11:10:59 +0530

    Add model rank ,suit and card

---
+++ src/main/java/com/amaap/pokerhandranking/domain/model/Card.java
@@ -0,0 +1,35 @@
+package com.amaap.pokerhandranking.domain.model;
+
+public class Card {
+    private final Suit suit;
+    private final Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return suit.getName() + rank.getName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Card card = (Card) o;
+
+        if (suit != card.suit) return false;
+        return rank == card.rank;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = suit.hashCode();
+        result = 31 * result + rank.hashCode();
+        return result;
+    }
+
+}
---
+++ src/main/java/com/amaap/pokerhandranking/domain/model/Suit.java
@@ -0,0 +1,29 @@
+package com.amaap.pokerhandranking.domain.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Suit {
+    HEARTS("H"), DIAMONDS("D"), CLUBS("C"), SPADES("S");
+
+    private final String keyword;
+    private static final Map<String, Suit> keywordMap = new HashMap<>();
+
+    static {
+        for (Suit suit : Suit.values()) {
+            keywordMap.put(suit.keyword, suit);
+        }
+    }
+
+    Suit(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public static Suit fromKeyword(String keyword) {
+        return keywordMap.get(keyword.toUpperCase());
+    }
+
+    public String getName() {
+        return keyword;
+    }
+}
