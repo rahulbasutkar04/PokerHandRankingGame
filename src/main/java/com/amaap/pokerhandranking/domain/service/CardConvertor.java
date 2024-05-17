73f1f44c5d42e620c2321c86ebab6e19212f2921
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-17 11:53:34 +0530

    Add Card Convertor Class

---
+++ src/main/java/com/amaap/pokerhandranking/domain/service/CardConvertor.java
@@ -0,0 +1,32 @@
+package com.amaap.pokerhandranking.domain.service;
+
+import com.amaap.pokerhandranking.domain.model.Card;
+import com.amaap.pokerhandranking.domain.model.Rank;
+import com.amaap.pokerhandranking.domain.model.Suit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardConvertor {
+
+    public List<Card> convertToCardList(List<String> hand) {
+        List<Card> cards = new ArrayList<>();
+        for (String cardStr : hand) {
+            if (cardStr.length() != 2) {
+                throw new IllegalArgumentException("Invalid card string: " + cardStr);
+            }
+            String suitStr = cardStr.substring(0, 1);
+            String rankStr = cardStr.substring(1);
+
+            Suit suit = Suit.fromKeyword(suitStr);
+            Rank rank = Rank.fromKeyword(rankStr);
+
+            if (suit == null || rank == null) {
+                throw new IllegalArgumentException("Invalid card string: " + cardStr);
+            }
+
+            cards.add(new Card(suit, rank));
+        }
+        return cards;
+    }
+}
