5fe2e0a90e41b8366d127ffe402c6c7367455f23
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-17 13:18:50 +0530

    update high card impl

--- src/main/java/com/amaap/pokerhandranking/domain/service/impl/HighCard.java
+++ src/main/java/com/amaap/pokerhandranking/domain/service/impl/HighCard.java
@@ -6,32 +6,20 @@ import com.amaap.pokerhandranking.domain.service.HandRanking;
 import java.util.List;
 
 public class HighCard implements HandRanking {
-    private  Card card;
+
     @Override
     public boolean evaluate(List<Card> hand) {
-        Card highestCard = hand.get(0);
-        for (int i = 1; i < hand.size(); i++) {
-            Card currentCard = hand.get(i);
-            if (currentCard.getRank().compareTo(highestCard.getRank()) > 0) {
-                highestCard = currentCard;
-            }
-        }
-
         return true;
     }
 
     public Card getHighestCard(List<Card> cardList) {
         Card highestCard = cardList.get(0);
-        for (int i = 1; i < cardList.size(); i++) {
+        for (int i = 0; i < cardList.size(); i++) {
             Card currentCard = cardList.get(i);
             if (currentCard.getRank().compareTo(highestCard.getRank()) > 0) {
                 highestCard = currentCard;
             }
         }
         return highestCard;
-    }
-
-    public Card getCard() {
-        return card;
     }
 }
