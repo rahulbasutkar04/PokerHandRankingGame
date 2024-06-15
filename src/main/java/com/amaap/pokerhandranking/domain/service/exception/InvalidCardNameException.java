79552004ddfc45c884724ead914c33d2b602113a
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-06-16 00:57:09 +0530

    Add custom exception for invalid card name

---
+++ src/main/java/com/amaap/pokerhandranking/domain/service/exception/InvalidCardNameException.java
@@ -0,0 +1,8 @@
+package com.amaap.pokerhandranking.domain.service.exception;
+
+public class InvalidCardNameException extends Exception {
+    public InvalidCardNameException(String s) {
+        super(s);
+
+    }
+}
