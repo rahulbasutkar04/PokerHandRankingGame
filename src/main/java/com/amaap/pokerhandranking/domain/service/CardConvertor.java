73f4d66ed88ce276989afcf7a06d36862bc6bbe0
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-06-16 00:57:48 +0530

    Update code

--- src/main/java/com/amaap/pokerhandranking/domain/service/CardConvertor.java
+++ src/main/java/com/amaap/pokerhandranking/domain/service/CardConvertor.java
@@ -3,17 +3,18 @@ package com.amaap.pokerhandranking.domain.service;
 import com.amaap.pokerhandranking.domain.model.Card;
 import com.amaap.pokerhandranking.domain.model.Rank;
 import com.amaap.pokerhandranking.domain.model.Suit;
+import com.amaap.pokerhandranking.domain.service.exception.InvalidCardNameException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CardConvertor {
 
-    public List<Card> convertToCardList(List<String> hand) {
+    public List<Card> convertToCardList(List<String> hand) throws InvalidCardNameException {
         List<Card> cards = new ArrayList<>();
         for (String cardStr : hand) {
             if (cardStr.length() != 2) {
-                throw new IllegalArgumentException("Invalid card string: " + cardStr);
+                throw new InvalidCardNameException("Invalid card string: " + cardStr);
             }
             String suitStr = cardStr.substring(0, 1);
             String rankStr = cardStr.substring(1);
@@ -22,7 +23,7 @@ public class CardConvertor {
             Rank rank = Rank.fromKeyword(rankStr);
 
             if (suit == null || rank == null) {
-                throw new IllegalArgumentException("Invalid card string: " + cardStr);
+                throw new InvalidCardNameException("Invalid card string: " + cardStr);
             }
 
             cards.add(new Card(suit, rank));
